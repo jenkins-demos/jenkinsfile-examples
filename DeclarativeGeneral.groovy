727a70286bfd94656062326132dfef8e368d8cdb
stubrowncloudbees <sbrown@cloudbees.com>
2019-08-11 12:54:07 +0100

    adding in stuff

---
+++ DeclarativeGeneral.groovy
@@ -0,0 +1,31 @@
+
+pipeline{
+    agent any
+    stages{
+        stage('build'){
+            steps{
+                echo 'building'
+               
+            }
+        }
+        stage('run-tests') {
+            parallel {
+                stage('testone') {
+                    steps {
+                        echo 'testing'
+                    }
+                }
+                stage('testtwo') {
+                    steps {
+                        echo 'testing'
+                    }
+                }
+            }
+        }
+        stage('package'){
+            steps {
+                echo 'packaging'
+            }
+        }
+    }
+}
