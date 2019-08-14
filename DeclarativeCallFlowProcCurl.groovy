c6630179c09f4939bcff57de9a7cc7e46a463b43
stubrowncloudbees <sbrown@cloudbees.com>
2019-08-14 11:25:51 +0100

    adding in seperate flow example

---
+++ DeclarativeCallFlowProcCurl.groovy
@@ -0,0 +1,27 @@
+@Library('shared-library') _
+pipeline{
+    agent any
+    environment {
+        FLOW_CREDS = credentials('flow-admin-creds')
+        FLOW_SVR_URL = credentials('flow-server-url')
+        PROJ_NAME = 'default'
+        PROC_NAME = 'Echo'
+        CURL_OPTIONS = '--insecure -vvv'
+
+    }
+    stages{
+
+        stage('deploy'){
+            environment{
+                json_args= '{"actualParameter":[{"actualParameterName":"arg1","value":"1234567"}]}'
+
+            }
+            steps{
+
+
+                runFlowProc(flowCreds: "${env.FLOW_CREDS_USR}:${env.FLOW_CREDS_PSW}", flowServer: "${env.FLOW_SVR_URL}" ,jsonArgs: '${json_args}',procName: '$PROC_NAME', projName: '$PROJ_NAME',curlOptions: '$CURL_OPTIONS')
+
+            }
+        }
+    }
+}
