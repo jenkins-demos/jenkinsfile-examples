e1467ae6912d248f699a86686cc26d3056381bda
stubrowncloudbees <sbrown@cloudbees.com>
2019-08-14 10:11:54 +0100

    moving json args

--- DeclarativeGeneralUsingLibs.groovy
+++ DeclarativeGeneralUsingLibs.groovy
@@ -5,7 +5,7 @@ pipeline{
         FLOW_CREDS = credentials('flow-admin-creds')
         FLOW_SVR_URL = credentials('flow-server-url')
         //json_args="{"actualParameter":[{"actualParameterName":"arg1","value":"1234567"}]}"
-        json_args= '{"actualParameter":[{"actualParameterName":"arg1","value":"1234567"}]}'
+        //json_args= '{"actualParameter":[{"actualParameterName":"arg1","value":"1234567"}]}'
 
     }
     stages{
@@ -35,6 +35,10 @@ pipeline{
         }
         stage('deploy'){
             steps{
+                environment{
+                    json_args= '{"actualParameter":[{"actualParameterName":"arg1","value":"1234567"}]}'
+
+                }
 
                 runFlowProc(flowCreds: "${env.FLOW_CREDS_USR}:${env.FLOW_CREDS_PSW}", flowServer: "${env.FLOW_SVR_URL}" ,jsonArgs: '${json_args}')
 
